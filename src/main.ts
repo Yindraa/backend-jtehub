72d73ec5f0ba6cf0542cd51c92020d1520e75fa4
galnoel <galnoelpeterrindengan@gmail.com>
2025-05-31 18:43:28 +0800

    fix: enable new url as origin cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000', // Local development
-      'https://stashify-pi.vercel.app' // Production frontend
+      'https://stashify-pi.vercel.app', // Production frontend
+      'https://framework-project-one.vercel.app/'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
